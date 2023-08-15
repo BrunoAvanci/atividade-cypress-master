eca544c0a66d545eddb273acd2b62c60f9552731
BrunoAvanci <136394314+BrunoAvanci@users.noreply.github.com>
2023-08-15 20:59:34 -0300

    testar-cadastro-login-meupc.net

---
+++ cypress/e2e/atividade-cypress.cy.js
@@ -0,0 +1,33 @@
+describe('Teste Cypress', () => {
+  it('Testar Cadastro', () => {
+
+    cy.visit('http://teste-cypress.s3-website-us-east-1.amazonaws.com/')
+
+    cy.get('a[href="cadastrar.html"]').click()
+
+    cy.get('input[type="text"]').type('Bruno Avanci')
+
+    cy.get('input[type="email"]').type('cypress@test.com')
+
+    cy.get('input[placeholder="senha"]').type('cypress')
+
+    cy.get('input[placeholder="Confirme a senha"]').type('cypress')
+
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('Você está logado!').should('be.visible')
+
+  })
+
+  it.only('Testar Login', () => {
+    cy.visit('http://teste-cypress.s3-website-us-east-1.amazonaws.com/')
+
+    cy.get('input[type="text"]').type('cypress@teste.com')
+
+    cy.get('input[type="password"]').type('cypress')
+
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('Você está logado!').should('be.visible')
+  })
+})
